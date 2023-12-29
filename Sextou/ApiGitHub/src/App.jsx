b82a4fbc49a2495f0a56c48f3932dfb5ebd6638c
Larissa-FLO <larissa.arqueo@outlook.com>
2023-12-29 17:29:28 -0300

    Adição de novo projeto

---
+++ Sextou/ApiGitHub/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import axios from 'axios'
+import './App.css'
+
+function App() {
+  const [usuario, setUsuario] = useState("")
+
+function handleBuscar(){
+
+axios.get(`https://api.github.com/users/${usuario}`).then(response => console.log(response.data))
+
+}
+
+  return (
+    <>
+    <div className='container'>
+      <div className='paginaPesquisa'>
+        <div className='busca'>
+          <input type="text" value={usuario} name="" id="" onChange={e => setUsuario(e.target.value)}/>
+          <button onClick={handleBuscar}>Pesquisar</button>
+        </div>
+        <div className='imagemBusca'>
+        {/*imagem de fundo*/}
+        </div>
+      </div>
+
+      <div className='resultadoPesquisa'>
+        <div className='header'>
+          <div className='fotoPerfil'>
+            {/*Foto de Perfil*/}
+          </div>
+          <div className='dadosPrincipais'>
+            <h2>{/*Nome*/}</h2>
+            <h4>{/*Usuário*/}</h4>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+      </>
+  )
+}
+
+export default App
